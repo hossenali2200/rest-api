58109b9119997af38af31679da8cacf1fb9a4d7d
hossenali2200 <hosenali2200@gmail.com>
2023-10-04 10:42:47 +0600

    restapi
    restapi

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const userRouter = require("./routes/user.route");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use("/api/users", userRouter);
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+ "/./views/index.html")
+  });
+
+app.use(( req, res, next)=>{
+  res.status(404).json({
+    message: "route not found",
+  });
+});
+
+//handeing server error
+app.use( ( err, req, res, next)=>{
+    res.status(500).json({
+      message: "something broke",
+    });
+  });
+
+
+ module.exports = app;
